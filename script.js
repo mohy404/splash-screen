98e41c4f37fe5c8b3df8a0999d7d6af43b3215fc
Mohay5 <Mohay.com>
2023-03-20 07:18:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+	setTimeout(function() {
+		document.querySelector('.splash-screen').style.display = 'none';
+	}, 8000); // Change this number to the amount of time you want the splash screen to display (in milliseconds).
+});
